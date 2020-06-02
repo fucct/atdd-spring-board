eca1ecc54b22eb3084667e36a45667320cd5d8fb
dd <dqrd123@gmail.com>
2020-06-02 18:46:40 +0900

    feat: Exception Logging 추가

--- src/main/java/spring/board/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/spring/board/demo/controller/GlobalExceptionHandler.java
@@ -8,19 +8,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
 import spring.board.demo.domain.error.dto.ErrorResponse;
 import spring.board.demo.exception.AccessDeniedException;
 import spring.board.demo.exception.BusinessException;
 import spring.board.demo.exception.EntityNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handlerArgument(MethodArgumentNotValidException e) {
+        log.debug("======================");
+        log.debug("Binding Validation Excetpion", e);
+        log.debug("======================");
+
         BindingResult bindingResult = e.getBindingResult();
         List<String> messages = bindingResult.getAllErrors()
             .stream()
@@ -35,25 +41,41 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e) {
+        log.debug("======================");
+        log.debug("Access Denied Exception", e);
+        log.debug("======================");
+
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
             .body(new ErrorResponse(e.getMessage(), HttpStatus.FORBIDDEN.value()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+        log.debug("======================");
+        log.debug("Entity Not Found Exception", e);
+        log.debug("======================");
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.debug("======================");
+        log.debug("Business Excetpion", e);
+        log.debug("======================");
+
         return ResponseEntity.badRequest()
-            .body(new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value()));
+            .body(new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+        log.debug("======================");
+        log.debug("UnExpected Excetpion", e);
+        log.debug("======================");
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 }
